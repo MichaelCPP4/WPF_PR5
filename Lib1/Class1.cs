c43b07e3179ed2db2a7a41c8a68df5c9c81f8143
исп-31 <-31@LAB17-01>
2022-10-24 14:11:29 +0300

    Добавьте файлы проекта.

---
+++ Lib1/Class1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib_1
+{
+    public class Car
+    {
+        string _marka;
+
+        int _numOfCylinders;
+
+        double _horsePower, _kW;
+
+        public string Marka
+        {
+            get => _marka;
+            set { if (value == "") _marka = null; else { _marka = value; } }
+        }
+
+        public double HorsePower
+        {
+            get => _horsePower;
+            set { if (value >= 0) _horsePower = value; }
+        }
+        public double KW
+        {
+            get => _kW;
+            set { if (value >= 0) _kW = value; }
+        }
+
+        public int NumOfCylinders
+        {
+            get => _numOfCylinders;
+            set { _numOfCylinders = value; }
+        }
+
+        public Car()
+        {
+            _marka = "Lada";
+            _numOfCylinders = 4;
+            _horsePower = 106;
+            _kW = 77.96;
+        }
+
+
+        /// <summary>
+        /// Метод меняет параметры автомобиля
+        /// </summary>
+        /// <param name="marka">Марка автомобиля</param>
+        /// <param name="numOfCylinders">Кол-во целиндров</param>
+        /// <param name="kW"></param>
+        public void SetParams(string marka, int numOfCylinders, double kW)
+        {
+            Marka = marka;
+            NumOfCylinders = numOfCylinders;
+            KW = kW;
+            HorsePower = kW * 1.36;
+        }
+
+        /// <summary>
+        /// Метод меняет параметры автомобиля
+        /// </summary>
+        /// <param name="marka">Марка автомобиля</param>
+        /// <param name="numOfCylinders">Кол-во целиндров</param>
+        public void SetParams(string marka, int numOfCylinders)
+        {
+            Marka = marka;
+            NumOfCylinders = numOfCylinders;
+        }
+
+        /// <summary>
+        /// Метод меняет параметры автомобиля
+        /// </summary>
+        /// <param name="marka">Марка автомобиля</param>
+        /// <param name="kW"></param>
+        public void SetParams(string marka, double kW)
+        {
+            Marka = marka;
+            KW = kW;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Lib_1;
+
+namespace WPF_PR5
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Car car = new Car();
+        //int kol = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void VivodButton_Click(object sender, RoutedEventArgs e)
+        {
+            textBoxOutLog.Text = $"Марка: {car.Marka ?? "Без названия"}"+
+                $"\nКол-во целиндров: {car.NumOfCylinders}"
+                +$"\nМощность двигателя в Л.С.: {car.HorsePower}"
+                +$"\nМощность двигателя в  кВт: {car.KW}";
+        }
+        private void ZapisButton_Click(object sender, RoutedEventArgs e)
+        {
+            double kW;
+            int numCylinders;
+
+            if (int.TryParse(textBoxNumCylinders.Text, out numCylinders) && double.TryParse(textBoxkW.Text, out kW))
+            {
+                car.SetParams(textBoxMarka.Text, numCylinders, kW);
+            }
+            else if (int.TryParse(textBoxNumCylinders.Text, out numCylinders))
+            {
+                car.KW = new double();
+                car.SetParams(textBoxMarka.Text, numCylinders);
+            }
+            else if (double.TryParse(textBoxkW.Text, out kW))
+            {
+                car.NumOfCylinders = new int();
+                car.SetParams(textBoxMarka.Text, kW);
+            }
+            else
+            {
+                car.SetParams(textBoxMarka.Text, new int(), new double());
+            }
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
